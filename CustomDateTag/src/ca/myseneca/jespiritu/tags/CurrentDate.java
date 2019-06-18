5357344124d2b9e8f2957a8b7de08bd0e7486441
Jeffrey Espiritu <jespiritu@myseneca.ca>
2019-06-18 14:29:06 -0400

    Add CustomDateTag project

---
+++ CustomDateTag/src/ca/myseneca/jespiritu/tags/CurrentDate.java
@@ -0,0 +1,18 @@
+
+package ca.myseneca.jespiritu.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import java.util.Date;
+
+
+public class CurrentDate extends SimpleTagSupport {
+	public void doTag() throws JspException, IOException {
+		JspWriter out = getJspContext().getOut();
+		out.print(new Date().toString());
+	}
+}
